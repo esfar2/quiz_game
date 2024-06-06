fbb2fd755ae9e3c5d12fe3c2eb3c6e422e6eb09b
esfar2 <esfarsami2@gmail.com>
2024-06-06 18:16:55 +0600

    added score content

--- quiz_game.py
+++ quiz_game.py
@@ -2,16 +2,17 @@ print("Welcome to my computer quiz!!!")
 
 playing = input("Do you want to play? yes or no ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     #quits the program and does not run it further 
     quit()
-
+score = 0
 print("Awesome!! Let's play")
 
 answer = input("What does CPU stands for? ")
 
 if answer.lower() == "central processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
     
@@ -20,6 +21,7 @@ answer = input("What does GPU stand for? ")
 
 if answer.lower() == "graphics processing unit":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
     
@@ -28,5 +30,8 @@ answer = input("What does RAM stand for? ")
 
 if answer.lower() == "random access memory":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
+    
+print(f"You got {str(score)} questions correct!!")
