27c96ef567b1f55676c53d3b463146be80c20fab
esfar2 <esfarsami2@gmail.com>
2024-06-06 18:12:12 +0600

    created the questionnaire

--- quiz_game.py
+++ quiz_game.py
@@ -7,3 +7,26 @@ if playing != "yes":
     quit()
 
 print("Awesome!! Let's play")
+
+answer = input("What does CPU stands for? ")
+
+if answer.lower() == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+    
+    
+answer = input("What does GPU stand for? ")
+
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+    
+    
+answer = input("What does RAM stand for? ")
+
+if answer.lower() == "random access memory":
+    print("Correct!")
+else:
+    print("Incorrect!")
