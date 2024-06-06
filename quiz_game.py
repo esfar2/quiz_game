522af48aba2372e1e405154fda1f91fce4722293
esfar2 <esfarsami2@gmail.com>
2024-06-06 18:05:35 +0600

    Ensured input is working properly

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,9 @@
-print("Hello Project!")
+print("Welcome to my computer quiz!!!")
+
+playing = input("Do you want to play? yes or no ")
+
+if playing != "yes":
+    #quits the program and does not run it further 
+    quit()
+
+print("Awesome!! Let's play")
